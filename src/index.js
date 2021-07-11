0ddf227c7e48344ef57f1570bd9a30cf862f2a3d
Stephen Le <stephensgamingguide@gmail.com>
2021-07-10 21:44:08 -0400

    testing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import ToolDisplay from "./index_usage.js"
-import UsageInDisplay from "./index_poke_data.js"
 import React from 'react';
 import * as smogon from 'smogon-usage-fetch'
 import ReactDOM from 'react-dom';
